447d73d7a503918b924a0bfcf1fc50cf25d8658c
danilo neves cruz <cruzdanilo@gmail.com>
2022-11-14 19:35:45 -0300

    ✨ market-update: post to slack

--- actions/onMarketUpdate.ts
+++ actions/onMarketUpdate.ts
@@ -1,13 +1,23 @@
+import fetch from 'cross-fetch';
 import { Interface } from '@ethersproject/abi';
 import type { ActionFn, TransactionEvent } from '@tenderly/actions';
 import type { MarketUpdateEventObject } from './types/Market';
+import MarketETHRouterABI from './abi/MarketETHRouter.json';
 import MarketABI from './abi/Market.json';
 
 const WAD = 10n ** 18n;
 
-const decoder = new Interface(MarketABI);
+const decoder = (() => {
+  const set = new Set();
+  return new Interface([...MarketABI, ...MarketETHRouterABI].filter(({ type, name, inputs }) => {
+    const key = `${type}:${name ?? ''}:${JSON.stringify(inputs?.map((i) => i.type))}`;
+    return !set.has(key) && set.add(key);
+  }));
+})();
 
-export default (async ({ storage }, { network, logs }: TransactionEvent) => {
+export default (async ({ storage, secrets }, {
+  network, logs, input, hash,
+}: TransactionEvent) => {
   for (const { address, data, topics } of logs) {
     let shareValue: bigint;
     try {
@@ -23,5 +33,23 @@ export default (async ({ storage }, { network, logs }: TransactionEvent) => {
     if (shareValue < await storage.getBigInt(key)) throw new Error(`${key} decreased`);
 
     await storage.putBigInt(key, shareValue);
+  }
+
+  try {
+    const { name } = decoder.parseTransaction({ data: input });
+    await fetch(await secrets.get(`SLACK_WHALE_ALERT@${network}`), {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        blocks: [
+          { type: 'header', text: { type: 'plain_text', text: name } },
+          { type: 'section', text: { type: 'plain_text', text: hash } },
+        ],
+      }),
+    });
+  } catch (error) {
+    if (error instanceof Error && error.message === 'SecretNotFound') return;
+
+    throw error;
   }
 }) as ActionFn;
--- test/onMarketUpdate.spec.ts
+++ test/onMarketUpdate.spec.ts
@@ -1,5 +1,8 @@
-import { TestRuntime } from '@tenderly/actions-test';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { expect, use } from 'chai';
+import { TestRuntime } from '@tenderly/actions-test';
+import type { SetupServerApi } from 'msw/node';
 import chaiAsPromised from 'chai-as-promised';
 import onMarketUpdate from '../actions/onMarketUpdate';
 import borrowPayload from './payloads/borrow.json';
@@ -8,8 +11,21 @@ use(chaiAsPromised);
 
 describe('on market update', () => {
   let runtime: TestRuntime;
+  let server: SetupServerApi;
 
-  beforeEach(() => { runtime = new TestRuntime(); });
+  before(() => {
+    server = setupServer(rest.post('https://web.hook', (_, res, ctx) => res(ctx.status(200))));
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  after(() => server.close());
+
+  beforeEach(() => {
+    runtime = new TestRuntime();
+    runtime.context.secrets.put('SLACK_WHALE_ALERT@1', 'https://web.hook');
+  });
 
   it('should store share value', async () => {
     await runtime.execute(onMarketUpdate, borrowPayload);
