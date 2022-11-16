e8b1b7684fcce2016c80ef49bc6a679d33577aa5
danilo neves cruz <cruzdanilo@gmail.com>
2022-11-16 17:54:13 -0300

    🔥 postinstall: generate only imported abis

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,10 +1,7 @@
 import { resolve } from 'path';
 import { runTypeChain } from 'typechain';
 import { writeFile, mkdir } from 'fs/promises';
-import InterestRateModelWETH from '@exactly-protocol/protocol/deployments/mainnet/InterestRateModelWETH.json';
-import MarketETHRouter from '@exactly-protocol/protocol/deployments/mainnet/MarketETHRouter.json';
 import MarketWETH from '@exactly-protocol/protocol/deployments/mainnet/MarketWETH.json';
-import Auditor from '@exactly-protocol/protocol/deployments/mainnet/Auditor.json';
 import DAI from '@exactly-protocol/protocol/deployments/mainnet/DAI.json';
 
 const rootDir = resolve(__dirname, '..');
@@ -16,10 +13,7 @@ const writeABI = async (path: string, abi: unknown[]) => {
 
 mkdir('actions/abi', { recursive: true }).then(async () => {
   const allFiles = await Promise.all([
-    writeABI('actions/abi/InterestRateModel.json', InterestRateModelWETH.abi),
-    writeABI('actions/abi/MarketETHRouter.json', MarketETHRouter.abi),
     writeABI('actions/abi/Market.json', MarketWETH.abi),
-    writeABI('actions/abi/Auditor.json', Auditor.abi),
     writeABI('actions/abi/ERC20.json', DAI.abi),
   ]);
   await runTypeChain({
