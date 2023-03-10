30d9da84221161b7059efa304b18eecbeb3f9266
danilo neves cruz <cruzdanilo@gmail.com>
2023-03-10 12:26:00 -0700

    ✨ utils: find market by predicate

--- actions/utils/findMarket.ts
+++ actions/utils/findMarket.ts
@@ -1,4 +1,7 @@
 import type { Previewer } from '../types/Previewer';
 
-export default (exactly: Previewer.MarketAccountStructOutput[], address: string) => exactly
-  .find(({ market }) => market.toLowerCase() === address.toLowerCase());
+export default (
+  exactly: Previewer.MarketAccountStructOutput[],
+  addressOrPredicate: string | ((market: Previewer.MarketAccountStructOutput) => boolean),
+) => exactly.find(typeof addressOrPredicate === 'function' ? addressOrPredicate
+  : ({ market }) => market.toLowerCase() === addressOrPredicate.toLowerCase());
