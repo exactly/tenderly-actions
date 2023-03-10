d6976724a94ffe6c35ad4a993fcc7248fb2dc267
danilo neves cruz <cruzdanilo@gmail.com>
2023-03-10 12:25:25 -0700

    ✨ utils: format usd

--- actions/utils/formatBigInt.ts
+++ actions/utils/formatBigInt.ts
@@ -5,11 +5,14 @@ export type Unit = number | string;
 export default (
   n: BigNumberish,
   symbol: Unit = 18,
-  decimals = { Ξ: 18, gwei: 9, '%': 18 }[symbol] ?? Number(symbol),
+  decimals = {
+    Ξ: 18, gwei: 9, $: 18, '%': 18,
+  }[symbol] ?? Number(symbol),
   maximumFractionDigits = {
-    Ξ: 9, gwei: 3, '%': 2, WETH: 5, DAI: 2, USDC: 2, WBTC: 5, wstETH: 5, OP: 2,
+    Ξ: 5, gwei: 3, $: 2, '%': 2, WETH: 5, DAI: 2, USDC: 2, WBTC: 5, wstETH: 5, OP: 2,
   }[symbol] ?? decimals,
 ) => `${(Number(n) / 10 ** decimals).toLocaleString(undefined, {
+  ...symbol === '$' && { style: 'currency', currency: 'USD' },
   ...symbol === '%' && { style: 'percent' },
   maximumFractionDigits,
-})}${typeof symbol === 'string' && symbol !== '%' ? ` ${symbol}` : ''}`;
+})}${typeof symbol === 'string' && !['$', '%'].includes(symbol) ? ` ${symbol}` : ''}`;
