2e5058f9de8d524d10bd61d40c95700e664190ec
danilo neves cruz <cruzdanilo@gmail.com>
2023-03-09 22:00:09 -0700

    💄 tweak decimal formatting

--- actions/utils/formatBigInt.ts
+++ actions/utils/formatBigInt.ts
@@ -7,7 +7,7 @@ export default (
   symbol: Unit = 18,
   decimals = { Ξ: 18, gwei: 9, '%': 18 }[symbol] ?? Number(symbol),
   maximumFractionDigits = {
-    Ξ: 4, gwei: 1, '%': 2, WETH: 4, DAI: 2, USDC: 2, WBTC: 5, wstETH: 4,
+    Ξ: 9, gwei: 3, '%': 2, WETH: 5, DAI: 2, USDC: 2, WBTC: 5, wstETH: 5, OP: 2,
   }[symbol] ?? decimals,
 ) => `${(Number(n) / 10 ** decimals).toLocaleString(undefined, {
   ...symbol === '%' && { style: 'percent' },
